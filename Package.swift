ff750f97441cdf8420a051e07a803fa55f330fe7
Paul Nettle <nettle.paul@gmail.com>
2022-09-13 10:58:32 -0500

    Updating to Xcode 14, updating swift lint settings

--- Package.swift
+++ Package.swift
@@ -3,19 +3,16 @@
 import Foundation
 import PackageDescription
 
-// For Mac M1's, we need to detect if these directories exist and if so, add them. This isn't technically necessary for the
-// include path, but the linker path will complain if we include a directory that doesn't exist. This is just to keep the
-// build output clean.
+// For Mac M1's, we need to detect if these directories exist and if so, add them. This isn't technically necessary
+// for the include path, but the linker path will complain if we include a directory that doesn't exist. This is
+// just to keep the build output clean.
 let kHomeBrewIncludeDir = "/opt/homebrew/include"
 let kHomeBrewLibDir = "/opt/homebrew/lib"
 
-func doesDirExist(_ dirPath: String) -> Bool
-{
+func doesDirExist(_ dirPath: String) -> Bool {
     var isDirectory: ObjCBool = false
-    if FileManager.default.fileExists(atPath: kHomeBrewIncludeDir, isDirectory: &isDirectory)
-    {
-        if isDirectory.boolValue
-        {
+    if FileManager.default.fileExists(atPath: kHomeBrewIncludeDir, isDirectory: &isDirectory) {
+        if isDirectory.boolValue {
             return true
         }
     }
@@ -25,8 +22,7 @@ func doesDirExist(_ dirPath: String) -> Bool
 var commonSwiftSettings: [SwiftSetting] {
     var result = [SwiftSetting]()
     result.append(.unsafeFlags(["-Ounchecked"], .when(configuration: .release)))
-    if doesDirExist(kHomeBrewIncludeDir)
-    {
+    if doesDirExist(kHomeBrewIncludeDir) {
         result.append(.unsafeFlags(["-I\(kHomeBrewIncludeDir)"]))
     }
 
@@ -36,8 +32,7 @@ var commonSwiftSettings: [SwiftSetting] {
 var commonLinkerSettings: [LinkerSetting] {
     var result = [LinkerSetting]()
     result.append(.unsafeFlags(["-L/usr/local/lib"]))
-    if doesDirExist(kHomeBrewLibDir)
-    {
+    if doesDirExist(kHomeBrewLibDir) {
         result.append(.unsafeFlags(["-L\(kHomeBrewLibDir)"]))
     }
 
@@ -47,20 +42,20 @@ var commonLinkerSettings: [LinkerSetting] {
 let commonCxxSettings: [CXXSetting] =
 [
     .define("NCURSES_OPAQUE", to: "0"),
-    .unsafeFlags(["-pthread", "-Wall", "-Wextra"]),
+    .unsafeFlags(["-pthread", "-Wall", "-Wextra"])
 ]
 
 let package = Package(
     name: "magic",
  	platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_15)
     ],
     products: [
         .executable(name: "whisper", targets: ["whisper"]),
         .executable(name: "mdscodes", targets: ["mdscodes"]),
         .library(name: "Seer", type: .dynamic, targets: ["Seer"]),
         .library(name: "Minion", type: .dynamic, targets: ["Minion"]),
-        .library(name: "NativeTasks", type: .static, targets: ["NativeTasks"]),
+        .library(name: "NativeTasks", type: .static, targets: ["NativeTasks"])
     ],
     targets: [
         .target(
@@ -106,7 +101,7 @@ let package = Package(
         .systemLibrary(name: "C_libpng", path: "Sources/Seer/SystemModules/C_libpng.build"),
         .systemLibrary(name: "C_AAlib", path: "Sources/whisper/SystemModules/C_AAlib.build"),
         .systemLibrary(name: "C_ncurses", path: "Sources/whisper/SystemModules/C_ncurses.build"),
-        .systemLibrary(name: "C_Libav", path: "Sources/whisper/SystemModules/C_Libav.build"),
+        .systemLibrary(name: "C_Libav", path: "Sources/whisper/SystemModules/C_Libav.build")
     ],
 
     cxxLanguageStandard: .cxx11
